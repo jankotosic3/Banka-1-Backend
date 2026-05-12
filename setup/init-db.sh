dff0eb0d31bcb37fbab88f7a814240fd73f95bcc
Aleksa Mojovic <mojovic.alexa@gmail.com>
2026-05-12 03:05:14 +0200

    chore(setup): aÅ¾urirane skripte za inicijalizaciju razvojnog okruÅ¾enja
    Usaglašeni skup setup skripti sa novom strukturom mikroservisa,
    ukljuÄujuÄ‡i izmene u Docker compose fajlovima i pomoÄ‡nim
    skriptama za inicijalizaciju baza podataka i konfiguracionih
    parametara.

--- setup/init-db.sh
+++ setup/init-db.sh
@@ -6,7 +6,6 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
     CREATE DATABASE banka;
     CREATE DATABASE notification_db;
     CREATE DATABASE clientdb;
-    CREATE DATABASE bankingdb;
     CREATE DATABASE accountdb;
     CREATE DATABASE card_db;
     CREATE DATABASE transferdb;
@@ -16,4 +15,12 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
     CREATE DATABASE orderdb;
     CREATE DATABASE stock_db;
     CREATE DATABASE credit_db;
+    CREATE DATABASE saga_db;
+    -- PR_19 C19.X: novi konsolidovani DB-ovi (user/banking-core/market/trading).
+    CREATE DATABASE user_service;
+    CREATE DATABASE banking_core;
+    CREATE DATABASE market_service;
+    CREATE DATABASE trading;
+    -- PR_32: interbank-service (X-Api-Key auth, inter-bank routing)
+    CREATE DATABASE interbank_service;
 EOSQL
